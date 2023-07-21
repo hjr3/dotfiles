7ec75d3c2287966262a4bd780c45214b939e3041
Herman J. Radtke III <herman@hermanradtke.com>
2023-07-21 15:42:58 -0400

    Stable copilot integration

--- .config/nvim/lua/custom/plugins/copilot.lua
+++ .config/nvim/lua/custom/plugins/copilot.lua
@@ -1,14 +1,27 @@
 -- copilot.lua
 --
--- Set up copilot
+-- Set up Github copilot
+-- Supports formatting and diagnostics via LSP
+-- https://www.reddit.com/r/neovim/comments/11ci5ik/copilot_is_no_longer_a_nuisance_for_me/
 
 return {
-  "zbirenbaum/copilot.lua",
+  'github/copilot.vim',
   config = function()
-    require("copilot").setup({
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-    })
+    vim.g.copilot_no_tab_map = true
+
+    local map = vim.keymap.set
+    --map('i', '[[', '<Plug>(copilot-next)', {})
+    --map('i', ']]', '<Plug>(copilot-previous)', {})
+    -- tab still works due to cmp (somehow?)
+    map('i', '^^', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+    vim.g.copilot_filetypes = {
+      ["*"] = false,
+      ["javascript"] = true,
+      ["typescript"] = true,
+      ["lua"] = false,
+      ["rust"] = true,
+    }
   end,
 }
 
